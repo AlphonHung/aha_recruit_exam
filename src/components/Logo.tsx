8fd6f949cc0c41f5c72774ff2dc802bdc9211c03
Desmond Hung <alphon.hung@gmail.com>
2022-05-26 00:18:44 +0800

    Logo link, default image, favicon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
 
 export function Logo() {
     const theme = useTheme();
     return (
-        <Typography
-            variant="h1"
-            component="div"
-            sx={{
-                fontSize: '0.65rem',
-                fontWeight: 700,
-                lineHeight: '15px',
-                color: 'transparent',
-                background: theme.custom.highlightLinear,
-                backgroundClip: 'text',
-            }}>LOGO
-        </Typography >
+        <Link to={'/'} style={{ textDecoration: 'none' }}>
+            <Typography
+                variant="h1"
+                component="div"
+                sx={{
+                    fontSize: '0.65rem',
+                    fontWeight: 700,
+                    lineHeight: '15px',
+                    color: 'transparent',
+                    background: theme.custom.highlightLinear,
+                    backgroundClip: 'text',
+                }}>LOGO
+            </Typography>
+        </Link>
     )
 }
--- src/helper/index.ts
+++
@@ -1,5 +0,0 @@
-// import { PurchaseHelper } from './purchase.helper';
-
-export const helper = {
-    // purchase: PurchaseHelper.instance,
-}
